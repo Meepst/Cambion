8bf8578453b2a17317f5cd5d5736eca8e3e61a27
James <james.li@unlv.edu>
2025-09-19 19:41:05 -0700

    Restrict common includes, change visibility of vector members

--- src/types.h
+++ src/types.h
@@ -5,18 +5,19 @@
 
 template <typename T>
 class Vector {
-    T* data;
-    uint64_t size;
+    public:
+        T* data;
+        uint64_t size;
 
-    Vector() = delete;
-    Vector(Allocator& allocator, uint64_t size, uint64_t alignment = 1):
-        data(alloc(allocator, size, alignment)), size(alignPow2(size, alignment)) {}
-    T& operator[](uint64_t i) {
-        if (i > size) {
-            printf("Error, Vector access out of bounds\n");
-            exit(-1);
+        Vector() = delete;
+        Vector(Allocator& allocator, uint64_t size, uint64_t alignment = 1):
+            data(alloc(allocator, size, alignment)), size(alignPow2(size, alignment)) {}
+        T& operator[](uint64_t i) {
+            if (i > size) {
+                printf("Error, Vector access out of bounds\n");
+                exit(-1);
+            }
+
+            return data[i];
         }
-
-        return data[i];
-    }
 };
